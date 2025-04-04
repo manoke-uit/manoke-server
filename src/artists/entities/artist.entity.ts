6b6b117b0ab4c1bee372a73c4dbda42f5bcb5620
Khong <hankhongg@gmail.com>
2025-04-04 19:39:09 +0700

    chore: declare artists, notifications, playlists entities

--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,1 +1,16 @@
-export class Artist {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('artists')
+export class Artist {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255 })
+    name: string;
+
+    @Column({ type: 'text' })
+    imageUrl: string;
+
+    @Column({ type: 'smallint'})
+    popularity: number;
+}
