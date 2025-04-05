010ef896313e3025df733260fe7a5a1fd26df75f
Khong <hankhongg@gmail.com>
2025-04-06 04:36:39 +0700

    refactor: modify dtos for input dtos

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,14 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID } from "class-validator";
-import { User } from "src/users/entities/user.entity";
 
 export class CreateNotificationDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: 'Notification title' })
     title: string;
 
     @IsString()
     @IsOptional()
+    @ApiProperty({ required: false, description: 'Notification description' })
     description?: string;
 
     @IsDateString()
@@ -17,13 +19,17 @@ export class CreateNotificationDto {
             const [day, month, year] = value.split('/');
             return `${year}-${month}-${day}`;
     })
+    @ApiProperty({ required: false, description: 'Notification creation date' })
     createdAt?: string; 
 
     @IsBoolean()
     @IsOptional() // default value is false
+    @ApiProperty({ required: false, description: 'Notification read status' })
     isRead?: boolean; 
 
     @IsNotEmpty()
     @IsUUID('4')
-    user: User;
+    @IsOptional()
+    @ApiProperty({ description: 'ID of the user who got the notification' })
+    userId: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,28 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Transform } from "class-transformer";
 import { IsArray, IsDate, IsDateString, IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword, IsUrl, IsUUID } from "class-validator";
-import { Notification } from "src/notifications/entities/notification.entity";
-import { Playlist } from "src/playlists/entities/playlist.entity";
-import { Score } from "src/scores/entities/score.entity";
 
 export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: 'User display name' })
     displayName: string; // @IsString() in dto
     
     @IsEmail()
     @IsNotEmpty()
+    @ApiProperty({ description: 'User email address' })
     email: string; // @IsEmail() in dto
 
     @IsOptional()
     @IsString()
+    @ApiProperty({ required: false, description: 'Admin secret for user creation' })
     adminSecret?: string; // @IsString() in dto
     
     @IsStrongPassword({ minLength: 8, minUppercase: 1, minLowercase: 1, minNumbers: 1, minSymbols: 1 }) // check password strength
     @IsNotEmpty()
+    @ApiProperty({ description: 'User password' })
     password: string; // @IsString() in dto
 
     @IsOptional()
     @IsUrl()
+    @ApiProperty({ required: false, description: 'User profile image URL' })
     imageUrl?: string; // @IsUrl() in dto
 
     @IsOptional()
@@ -31,20 +34,24 @@ export class CreateUserDto {
         const [day, month, year] = value.split('/');
         return `${year}-${month}-${day}`;
     })
+    @ApiProperty({ required: false, description: 'User creation date' })
     createdAt?: string; // @IsDateString() in dto
 
-    @IsArray()
-    @IsUUID('4', { each: true })
     @IsOptional()
-    notifications?: Notification[]; // @IsArray() in dto
+    @IsUUID('4', { each: true })
+    @IsArray()
+    @ApiProperty({ required: false, description: 'User notifications' })
+    notificationIds?: string[]; // @IsArray() in dto
+    
+    @IsOptional()
+    @IsUUID('4', { each: true })
+    @IsArray()
+    @ApiProperty({ required: false, description: 'User playlists' })
+    playlistIds?: string[]; // @IsArray() in dto
 
     @IsOptional()
-    @IsArray()
     @IsUUID('4', { each: true })
-    playlists?: Playlist[]; // @IsArray() in dto
-
-    @IsOptional()
     @IsArray()
-    @IsUUID('4', { each: true })
-    scores?: Score[]; // @IsArray() in dto
+    @ApiProperty({ required: false, description: 'User scores' })
+    scoreIds: string[]; // @IsArray() in dto
 }
