2770d364ca206696f22894e21ace93b58b4b3e06
Khong <hankhongg@gmail.com>
2025-04-05 16:16:48 +0700

    chore: add and config dtos

--- src/friends/dto/create-friend.dto.ts
+++ src/friends/dto/create-friend.dto.ts
@@ -1,1 +1,20 @@
-export class CreateFriendDto {}
+import { FriendStatus } from "../entities/friend.entity";
+import { IsDateString, IsEnum, IsNotEmpty, IsOptional, IsUUID } from "class-validator";
+
+export class CreateFriendDto {
+    @IsUUID('4')
+    @IsNotEmpty()
+    userId_1: string; // @IsUUID() in dto
+
+    @IsNotEmpty()
+    @IsUUID('4')
+    userId_2: string; // @IsUUID() in dto
+
+    @IsNotEmpty()
+    @IsEnum(FriendStatus)
+    status: FriendStatus
+
+    @IsOptional()
+    @IsDateString()
+    createdAt?: string; // @IsDateString() in dto
+}
