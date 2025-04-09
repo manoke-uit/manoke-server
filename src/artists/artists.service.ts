fba27e6845b4178e9f51b7ffa1fef41917640a6b
Tien Ngo <quangtienngo661@gmail.com>
2025-04-09 19:28:23 +0700

    pull

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Artist } from './entities/artist.entity';
 
 @Injectable()
 export class ArtistsService {
+  constructor(
+    @InjectRepository(Artist) 
+    private artistRepository: Repository<Artist>
+  ) {}
+
   create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+    
   }
 
   findAll() {
