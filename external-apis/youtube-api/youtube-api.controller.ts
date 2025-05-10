3b6fde452b952b548415ad095cf0a1402e3a35b3
Khong <hankhongg@gmail.com>
2025-05-10 15:52:45 +0700

    feat: last commit before redoing backend db

--- external-apis/youtube-api/youtube-api.controller.ts
+++ external-apis/youtube-api/youtube-api.controller.ts
@@ -6,7 +6,7 @@ import { YoutubeSearchResponseDto } from 'external-apis/dto/youtube-response.dto
 export class YoutubeApiController {
     constructor(private youtubeApiService : YoutubeApiService) {}
     @Get('search')
-    async searchVideos(@Query('q') query: string, @Query('pageToken') pageToken?: string,) : Promise<YoutubeSearchResponseDto> {
+    async searchVideos(@Query('query') query: string, @Query('pageToken') pageToken?: string,) : Promise<YoutubeSearchResponseDto> {
         return await this.youtubeApiService.searchVideos(query,5, pageToken);
     }
 }
