eb82bf1d134fbfb7e079437cc5e39f6c99ffba99
Khong <hankhongg@gmail.com>
2025-06-08 10:20:01 +0700

    fix: comment module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from 'src/posts/entities/post.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Comment } from './entities/comment.entity';
+import { Friend } from 'src/friends/entities/friend.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, User, Comment, Post])],
+  imports: [TypeOrmModule.forFeature([Post, User, Comment, Post, Friend])],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
